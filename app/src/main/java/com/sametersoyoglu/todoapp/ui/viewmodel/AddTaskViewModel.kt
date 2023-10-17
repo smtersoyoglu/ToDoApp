71052168e11ebf330d10f5f36527a3061c253d40
smter <smtersoyoglu231@gmail.com>
2023-10-18 00:18:20 +0300

    Room

--- app/src/main/java/com/sametersoyoglu/todoapp/ui/viewmodel/AddTaskViewModel.kt
+++ app/src/main/java/com/sametersoyoglu/todoapp/ui/viewmodel/AddTaskViewModel.kt
@@ -14,7 +14,6 @@ import javax.inject.Inject
 
 @HiltViewModel
 class AddTaskViewModel @Inject constructor(var taskRepository : TaskRepository) : ViewModel() {
-
     fun save(task_title:String,task_description:String) {
         CoroutineScope(Dispatchers.Main).launch {
             taskRepository.save(task_title,task_description)
