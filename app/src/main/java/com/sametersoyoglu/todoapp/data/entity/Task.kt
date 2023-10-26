164fa9b9fc69c0ec6bd022f10d0d5febb13087e8
smter <smtersoyoglu231@gmail.com>
2023-10-26 21:07:21 +0300

    Room

--- app/src/main/java/com/sametersoyoglu/todoapp/data/entity/Task.kt
+++ app/src/main/java/com/sametersoyoglu/todoapp/data/entity/Task.kt
@@ -3,11 +3,12 @@ package com.sametersoyoglu.todoapp.data.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import org.jetbrains.annotations.NotNull
 import java.io.Serializable
 
 @Entity(tableName = "task")
 data class Task (@PrimaryKey(autoGenerate = true)
-                 @ColumnInfo(name = "task_id") var task_id : Int,
-                 @ColumnInfo(name = "task_title") var task_title:String,
-                 @ColumnInfo(name = "task_description") var task_description:String,
-                 @ColumnInfo(name = "task_date") var task_date:String) : Serializable
+                 @ColumnInfo(name = "task_id") @NotNull var task_id : Int,
+                 @ColumnInfo(name = "task_title") @NotNull var task_title:String,
+                 @ColumnInfo(name = "task_description") @NotNull var task_description:String,
+                 @ColumnInfo(name = "task_date") @NotNull var task_date:String) : Serializable
